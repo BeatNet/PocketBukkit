fc4229b38f8b47848a20372cce02086958d179db
Philip Shilling <comedyman937@yahoo.com>
2014-07-24 15:23:52 -0700

    Server Starting Method

---
+++ PocketBukkit/src/net/pocketbukkit/PocketBukkit.java
@@ -0,0 +1,21 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package net.pocketbukkit;
+
+public class PocketBukkit {
+	public static void main(String[] args){
+		try{
+			Server server = new Server();
+			server.run();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
