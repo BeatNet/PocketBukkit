db6351884ab6ba13cc2d8e732c58cc9801b2ff97
Philip Shilling <comedyman937@yahoo.com>
2014-07-24 18:49:09 -0700

    Air Block

---
+++ PocketBukkit/src/net/pocketbukkit/block/blocks/Air.java
@@ -0,0 +1,32 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package net.pocketbukkit.block.blocks;
+
+import net.pocketbukkit.block.Block;
+
+public class Air {
+	
+	public static void __construct(){
+		boolean isActivable = false;
+		boolean breakable = false;
+		boolean isFlowable = true;
+		boolean isTransparent = true;
+		boolean isReplaceable = true;
+		boolean isPlaceable = false;
+		boolean hasPhysics = false;
+		boolean isSolid = false;
+		boolean isFullBlock = true;
+		int hardness = 0;
+	}
+	
+	public static void getBoundingBox(){
+		//TODO
+	}
+}
