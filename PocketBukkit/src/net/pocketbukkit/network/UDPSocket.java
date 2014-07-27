0a2252b655032f11504b0ab194418220b9a577b0
Philip Shilling <comedyman937@yahoo.com>
2014-07-27 05:35:34 -0700

    Started over on UDP

--- PocketBukkit/src/net/pocketbukkit/network/UDPSocket.java
+++ PocketBukkit/src/net/pocketbukkit/network/UDPSocket.java
@@ -1,42 +1,16 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
 package net.pocketbukkit.network;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-
-import net.pocketbukkit.utility.ServerLogger;
-
 public class UDPSocket {
-	private static DatagramSocket serverSocket;
-	private static ServerLogger logger;
-
-	public static void open() throws Exception{ 
-		try{ 
-			serverSocket = new DatagramSocket(19132); 
-			byte[] receiveData = new byte[1024]; 
-			byte[] sendData  = new byte[1024]; 
-			while(true){ 
-				receiveData = new byte[1024]; 
-				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
-				logger.info("Waiting for datagram packet...");
-				serverSocket.receive(receivePacket); 
-				String sentence = new String(receivePacket.getData()); 
-				InetAddress IPAddress = receivePacket.getAddress(); 
-				int port = receivePacket.getPort(); 
-				logger.info("From: " + IPAddress + ":" + port);
-				logger.info("Message: " + sentence);
-				String capitalizedSentence = sentence.toUpperCase(); 
-				sendData = capitalizedSentence.getBytes(); 
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); 
-				serverSocket.send(sendPacket); 
-			} 
-		}catch(SocketException ex){
-			System.exit(1);
-		}
-	}
-	
-	public static void close(){
-		serverSocket.close();
+	public static void main(String[] args){
+		
 	}
 }
