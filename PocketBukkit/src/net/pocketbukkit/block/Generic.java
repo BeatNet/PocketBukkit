fb2ac1bb13541361194d8031816a3b5fa5dc6ac1
Philip Shilling <comedyman937@yahoo.com>
2014-07-27 05:26:31 -0700

    Create Generic.java

---
+++ PocketBukkit/src/net/pocketbukkit/block/Generic.java
@@ -0,0 +1,25 @@
+package net.pocketbukkit.block;
+
+public class Generic {
+	public static void main(String[] args){}
+
+	public void place(){
+		//TODO: Placing Blocks
+	}
+
+	public static boolean isBreakable(boolean isBreakable){
+		return isBreakable;
+	}
+
+	public void onBreak(){
+		//TODO: Breaking Blocks
+	}
+
+	public void onUpdate(){
+		//TODO: Block Updates
+	}
+
+	public boolean onActivate(boolean isActivable){
+		return isActivable;
+	}
+}
