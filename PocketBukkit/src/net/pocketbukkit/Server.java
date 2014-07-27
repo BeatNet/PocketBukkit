f3866b80339e92a53bf7ec759a39665415076ac7
Philip Shilling <comedyman937@yahoo.com>
2014-07-27 05:50:03 -0700

    Added Tags

--- PocketBukkit/src/net/pocketbukkit/Server.java
+++ PocketBukkit/src/net/pocketbukkit/Server.java
@@ -35,12 +35,12 @@ public class Server {
 	public void run() throws IOException {
 		startStopServer();
 		serverRunning = true;
-		Server.logger.info("Server starting on: *:" + getServerPort() + ", on Minecraft Version: " + MinecraftVersion);
-		Server.logger.info("This server is running PocketBukkit version " + version + " (" + codename + ") (" + api + ")");
+		Server.logger.info("[PocketBukkit] Server starting on: *:" + getServerPort() + ", on Minecraft Version: " + MinecraftVersion);
+		Server.logger.info("[PocketBukkit] This server is running PocketBukkit version " + version + " (" + codename + ") (" + api + ")");
 		checkFiles();
 		enablePlugins();
 		loadWorlds();
-		Server.logger.info("Done!");
+		Server.logger.info("[PocketBukkit] Done!");
 	}
 
 	public static String generateString(Random rng, String characters, int length) { 
@@ -51,31 +51,31 @@ public class Server {
 	 }
 	
 	public void checkFiles() throws IOException{
-		Server.logger.info("Loading server properties...");
+		Server.logger.info("[PocketBukkit] Loading server properties...");
 		/* Create the files */
 		File bannedips = new File("banned-ips.txt");
 		if(!bannedips.exists()){
-			Server.logger.info("No banned ips file found! Creating one...");
+			Server.logger.info("[PocketBukkit] No banned ips file found! Creating one...");
 			bannedips.createNewFile();
 		}
 		File bannedplayers = new File("banned-players.txt");
 		if(!bannedplayers.exists()){
-			Server.logger.info("No banned players file found! Creating one...");
+			Server.logger.info("[PocketBukkit] No banned players file found! Creating one...");
 			bannedplayers.createNewFile();
 		}
 		File ops = new File("ops.txt");
 		if(!ops.exists()){
-			Server.logger.info("No ops file found! Creating one...");
+			Server.logger.info("[PocketBukkit] No ops file found! Creating one...");
 			ops.createNewFile();
 		}
 		File yaml = new File("PocketBukkit.yml");
 		if(!yaml.exists()){
-			Server.logger.info("No PocketBukkit YAML found! Creating one...");
+			Server.logger.info("[PocketBukkit] No PocketBukkit YAML found! Creating one...");
 			yaml.createNewFile();
 		}
 		File properties = new File("server.properties");
 		if(!properties.exists()){
-			Server.logger.info("No properties file found! Creating one...");
+			Server.logger.info("[PocketBukkit] No properties file found! Creating one...");
 			try {
 				server.setProperty("force-gamemode", "false");
 				server.setProperty("auto-save", "true");
@@ -111,24 +111,24 @@ public class Server {
 		}
 		File log = new File("server.log");
 		if(!log.exists()){
-			Server.logger.info("No server log found! Creating one...");
+			Server.logger.info("[PocketBukkit] No server log found! Creating one...");
 			log.createNewFile();
 		}
 		
 		/* Create the directories */
 		File players = new File("players/");
 		if(!players.exists()){
-			Server.logger.info("No players folder found! Creating one...");
+			Server.logger.info("[PocketBukkit] No players folder found! Creating one...");
 			players.mkdir();
 		}
 		File plugins = new File("plugins/");
 		if(!plugins.exists()){
-			Server.logger.info("No plugins folder found! Creating one...");
+			Server.logger.info("[PocketBukkit] No plugins folder found! Creating one...");
 			plugins.mkdir();
 		}
 		File worlds = new File("worlds/");
 		if(!worlds.exists()){
-			Server.logger.info("No worlds folder found! Creating one...");
+			Server.logger.info("[PocketBukkit] No worlds folder found! Creating one...");
 			worlds.mkdir();
 			Level.createDefaultWorld();
 		}else{
@@ -143,12 +143,12 @@ public class Server {
 	}
 
 	public void enablePlugins(){
-		Server.logger.info("Loading plugins...");
+		Server.logger.info("[PocketBukkit] Loading plugins...");
 		//TODO: Check the plugins directory for any files and try to load the proper JAR files
 	}
 	
 	public void loadWorlds(){
-		Server.logger.info("Loading worlds...");
+		Server.logger.info("[PocketBukkit] Loading worlds...");
 		//TODO: Load levels (start with default level) and generate them if non existent
 	}
 
@@ -159,7 +159,7 @@ public class Server {
 		if(serverPort == "19132" || serverPort == "19133" || serverPort == "19134" || serverPort == "19135" || serverPort == "19136" || serverPort == "19137" || serverPort == "19138" || serverPort == "19139" || serverPort == "19140" || serverPort == "19141" || serverPort == "19142" || serverPort == "19143" || serverPort == "19144" || serverPort == "19145"){
 			
 		}else{
-			logger.fatal("Server port must be between 19132 and 19145!");
+			logger.fatal("[PocketBukkit] Server port must be between 19132 and 19145!");
 		}
 		return serverPort;
 		*/
@@ -171,7 +171,7 @@ public class Server {
 			thread.start();
 			TCPSocket.openTCPPort();
 		} catch (IOException e) {
-			Server.logger.info("There was a IOException! Printing stack trace:");
+			Server.logger.info("[PocketBukkit] There was a IOException! Printing stack trace:");
 			e.printStackTrace();
 		}
 	}
