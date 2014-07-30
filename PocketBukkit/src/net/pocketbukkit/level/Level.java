cb2a5ccd1f2211de847f1bc2b4c31e0f7d656bcf
Philip Shilling <comedyman937@yahoo.com>
2014-07-30 08:53:43 -0700

    Update Level.java

--- PocketBukkit/src/net/pocketbukkit/level/Level.java
+++ PocketBukkit/src/net/pocketbukkit/level/Level.java
@@ -10,25 +10,49 @@
 package net.pocketbukkit.level;
 
 import java.io.File;
+import java.io.IOException;
+
 import net.pocketbukkit.Server;
 import net.pocketbukkit.utility.ServerLogger;
 
 public class Level {
 	private static final ServerLogger logger = new ServerLogger();
 
-	public static void createDefaultWorld() {
+	public static void createDefaultLevel() {
 		String level = (String) (Server.server.getProperty("level-name"));
 		logger.info("[PocketBukkit] Generating new world...");
 		File createDefaultLevel = new File("worlds/" + level);
 		createDefaultLevel.mkdir();
 		generateChunks(level);
 	}
-
-	public static void loadDefaultLevel() {
+	
+	public static void createNewLevel(){
 		
 	}
 
 	private static void generateChunks(String level) {
+		File data = new File("worlds/" + level + "/level.dat");
+		Server.logger.info("[PocketBukkit] No ops file found! Creating one...");
+		try {
+			data.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		File region = new File("worlds/" + level + "/region/");
+		region.mkdir();
+		//TODO: Generate the chunks
+	}
+
+	public static void loadDefaultLevel() {
+		//TODO: Load the default world
+	}
+	
+	public static void loadOtherLevels(){
 		
+	}
+	
+	public static void loadAllLevels(){
+		loadDefaultLevel();
+		loadOtherLevels();
 	}
 }
