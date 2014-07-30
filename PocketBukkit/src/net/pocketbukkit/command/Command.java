b1c45a907c80db2301de39cf32cea6574ee2ea15
Philip Shilling <comedyman937@yahoo.com>
2014-07-30 08:54:28 -0700

    Update Command.java

--- PocketBukkit/src/net/pocketbukkit/command/Command.java
+++ PocketBukkit/src/net/pocketbukkit/command/Command.java
@@ -9,6 +9,30 @@
 
 package net.pocketbukkit.command;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import net.pocketbukkit.utility.ServerLogger;
+
 public class Command {
+	private static ServerLogger logger;
+	public static String Command;
+	public static boolean RequireOP = false;
+	static BufferedReader ops;
 	
+	public static void onCommand(){
+		if(RequireOP){
+			try {
+				ops = new BufferedReader(new FileReader("ops.txt"));
+				try {
+					ops.readLine();
+				} catch (IOException e) {
+					Sender.sendMessage("You do not have permission to do that.");
+				}
+			} catch (FileNotFoundException e) {
+				logger.fatal("Unable to find the ops.txt file!");
+			}
+		}
+	}
 }
