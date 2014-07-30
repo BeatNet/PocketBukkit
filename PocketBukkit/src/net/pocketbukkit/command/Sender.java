f1bd2f982c064798849eb081e47d300fd9b60a53
Philip Shilling <comedyman937@yahoo.com>
2014-07-30 08:55:43 -0700

    Start Sender Implementation

---
+++ PocketBukkit/src/net/pocketbukkit/command/Sender.java
@@ -0,0 +1,20 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package net.pocketbukkit.command;
+
+public class Sender {
+	public static void main(String[] args){
+		
+	}
+	
+	public static void sendMessage(String message){
+		
+	}
+}
