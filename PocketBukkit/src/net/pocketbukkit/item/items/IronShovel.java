d612cf94fb85c85515b0694358908854fa80bc7c
Philip Shilling <comedyman937@yahoo.com>
2014-08-01 12:03:21 -0700

    Create IronShovel.java

---
+++ PocketBukkit/src/net/pocketbukkit/item/items/IronShovel.java
@@ -0,0 +1,19 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package net.pocketbukkit.item.items;
+
+import net.pocketbukkit.item.Item;
+import net.pocketbukkit.item.Tool;
+
+public class IronShovel extends Tool implements Item {
+	public static void main(String[] args){
+		int strength = 1;
+	}
+}
