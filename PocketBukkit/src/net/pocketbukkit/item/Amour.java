09bff94cda7d99720c4af7038dc01045f765ca3f
Philip Shilling <comedyman937@yahoo.com>
2014-08-01 12:16:43 -0700

    Added "isEdible"

--- PocketBukkit/src/net/pocketbukkit/item/Amour.java
+++ PocketBukkit/src/net/pocketbukkit/item/Amour.java
@@ -4,6 +4,7 @@ public class Armour {
 	public static void main(String[] args){
 		boolean isPlacable = false;
 		boolean isWearable = true;
+		boolean isEdible = false;
 		boolean isObtainable = true;
 		boolean isDropable = true;
 		boolean requiresOP = false;
