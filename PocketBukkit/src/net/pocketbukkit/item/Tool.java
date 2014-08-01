891dba8df64fdf05716c6efd5509df5814cd5ddf
Philip Shilling <comedyman937@yahoo.com>
2014-08-01 12:05:45 -0700

    Create Tool.java

---
+++ PocketBukkit/src/net/pocketbukkit/item/Tool.java
@@ -0,0 +1,20 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_| _ \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_| _ \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package net.pocketbukkit.item;
+
+public class Tool {
+	public static void main(String[] args){
+		boolean isPlacable = false;
+		boolean isWearable = false;
+		boolean isObtainable = true;
+		boolean isDropable = true;
+		boolean requiresOP = false;
+	}
+}
