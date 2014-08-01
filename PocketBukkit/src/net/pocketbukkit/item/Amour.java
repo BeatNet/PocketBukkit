e21dff12b32b6f710eeb0234d989b2c6b87c63ca
Philip Shilling <comedyman937@yahoo.com>
2014-08-01 12:13:18 -0700

    Create Amour.java

---
+++ PocketBukkit/src/net/pocketbukkit/item/Amour.java
@@ -0,0 +1,11 @@
+package net.pocketbukkit.item;
+
+public class Armour {
+	public static void main(String[] args){
+		boolean isPlacable = false;
+		boolean isWearable = true;
+		boolean isObtainable = true;
+		boolean isDropable = true;
+		boolean requiresOP = false;
+	}
+}
