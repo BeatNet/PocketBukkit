d4be97c9ce9ce8d80f4f362bfbf77f0c65512f92
Philip Shilling <comedyman937@yahoo.com>
2014-08-11 13:59:06 -0500

    Started MySQL API

---
+++ PocketBukkit/src/net/pocketbukkit/utility/MySQL.java
@@ -0,0 +1,19 @@
+package net.pocketbukkit.utility;
+
+pubic class MySQL {
+  public static void connect() {
+    //TODO: Connect to database
+  }
+  
+  public static void ping() {
+    //TODO: Ping database
+  }
+  
+  public static void execute(String command) {
+    //TODO: Execute command
+  }
+  
+  public static void close() {
+    //TODO: Close connection
+  }
+}
