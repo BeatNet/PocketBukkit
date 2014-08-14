b94ff0ead45d8239a0589d6e1d1afc7c1a9c3ca6
Philip Shilling <comedyman937@yahoo.com>
2014-08-13 18:35:25 -0700

    Fixed some errors / Some still exist (On Windows)

--- PocketBukkit/start.bat
+++ PocketBukkit/start.bat
@@ -1,52 +1,66 @@
 @echo off
-Stay_Updated="no"
+set Stay_Updated=no
 
 Title PocketBukkit Console
 Cls
-IF %Stay_Updated% == "yes" (
-  PB="download"
+IF %Stay_Updated% == yes (
+  set PB=download
 ) ELSE (
-  PB=""
+  set PB=determine
 )
 
-IF EXIST PocketBukkit.jar (
-  PB="jar"
-) ELSE (
-  IF EXIST src/net/pocketbukkit/PocketBukkit.class (
-    PB="runsrc"
+IF %PB% == determine (
+  IF EXIST PocketBukkit.jar (
+    set PB=jar
   ) ELSE (
-    IF EXIST src/net/pocketbukkit/PocketBukkit.java (
-      PB="compile"
+    IF EXIST src/net/pocketbukkit/PocketBukkit.class (
+      set PB=runsrc
     ) ELSE (
-      PB="download"
-    )
-  )
-)
-
-IF %PB% == "jar" (
-  java -jar PocketBukkit.jar
-) ELSE (
-  IF %PB% == "compile" (
-    echo "[WARNING] [PocketBukkit] PocketBukkit needs to be compiled!"
-    echo "[1/2] Comipling PocketBukkit..."
-    javac src/net/pocketbukkit/*.java
-    javac src/net/pocketbukkit/*/*.java
-    javac src/net/pocketbukkit/*/*/*.java
-    echo "Done!"
-    echo "[2/2] Starting PocketBukkit..."
-    java src/net/pocketbukkit/PocketBukkit.class
-  ) ELSE (
-    IF %PB% == "runsrc" (
-      java src/net/pocketbukkit/PocketBukkit.class
-    ) ELSE (
-      IF %PB% == "download" (
-        echo "Downloading has not yet been implemented for the batch startup!"
-        Pause
+      IF EXIST src/net/pocketbukkit/PocketBukkit.java (
+        set PB=compile
       ) ELSE (
-        echo "[FATAL] [PocketBukkit] Unable to determine required function!"
-        Pause
+        set PB=download
       )
     )
   )
 )
-Exit
+
+IF %PB% == jar (
+  java -jar PocketBukkit.jar
+) ELSE (
+  IF %PB% == compile (
+    ECHO [WARNING] [PocketBukkit] PocketBukkit needs to be compiled!
+    ECHO [1/2] Compiling PocketBukkit...
+    javac src/net/pocketbukkit/*.java
+    javac src/net/pocketbukkit/*/*.java
+    javac src/net/pocketbukkit/*/*/*.java
+    ECHO Done!
+    ECHO [2/2] Starting PocketBukkit...
+    java src/net/pocketbukkit/PocketBukkit.class
+  ) ELSE (
+    IF %PB% == download (
+      IF EXIST start.sh (
+        del start.sh
+      )
+      IF EXIST PocketBukkit.jar (
+        del PocketBukkit.jar
+      )
+      IF EXIST src/net/pocketbukkit/PocketBukkit.java (
+        rmdir src/
+      )
+      IF EXIST src/net/pocketbukkit/PocketBukkit.class (
+        rm src/
+      )
+      XCOPY http://pocketbukkit.net/download/0001/PocketBukkit.jar
+      java -jar PocketBukkit.jar
+    ) ELSE (
+      IF %PB% == runsrc (
+        java src/net/pocketbukkit/PocketBukkit.class
+      ) ELSE (
+        ECHO [FATAL] [PocketBukkit] Unable to determine required operation!
+        PAUSE
+      )
+    )
+  )
+)
+EXIT
