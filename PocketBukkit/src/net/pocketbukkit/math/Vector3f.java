2b6b999027476e0ea06e387e337d7a71d5e1942c
Comedyman937 <comedyman937@yahoo.com>
2014-09-04 13:29:43 -0400

    Added Some Math

---
+++ PocketBukkit/src/net/pocketbukkit/math/Vector3f.java
@@ -0,0 +1,19 @@
+package net.pocketbukkit.math;
+
+public class Vector3f {
+public static float vectorx;
+public static float vectory;
+public static float vectorz;
+	
+	public static void vector3f() {
+		vectorx = 0.0f;
+		vectory = 0.0f;
+		vectorz = 0.0f;
+	}
+	
+	public static void vector3f(float x, float y, float z) {
+		vectorx = x;
+		vectory = y;
+		vectorz = z;
+	}
+}
