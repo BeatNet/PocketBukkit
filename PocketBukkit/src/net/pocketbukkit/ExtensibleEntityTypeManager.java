2adf2388345ecfab3c65737f9cb128e8ae0d4cdd
PEMapModder <pemapmodder1970@gmail.com>
2014-10-22 16:30:15 +0800

    Added dummy BlockServer interaction classes

---
+++ PocketBukkit/src/net/pocketbukkit/ExtensibleEntityTypeManager.java
@@ -0,0 +1,12 @@
+package net.pocketbukkit;
+
+import org.blockserver.entity.EntityType;
+import org.blockserver.entity.EntityTypeManager;
+
+public class ExtensibleEntityTypeManager implements EntityTypeManager{
+	@Override
+	public EntityType<?> getType(Byte b){
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ PocketBukkit/src/net/pocketbukkit/PocketBukkitChatManager.java
@@ -0,0 +1,7 @@
+package net.pocketbukkit;
+
+import org.blockserver.chat.SimpleChatManager;
+
+public class PocketBukkitChatManager extends SimpleChatManager{
+	// TODO Auto-generated class stub
+}
